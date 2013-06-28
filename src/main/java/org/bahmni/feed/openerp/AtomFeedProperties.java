1250a9111ecf3eed9a82d869ca340a47c4e02b31
Mário Areias <mario.s.s.areias@gmail.com>
2013-06-28 11:45:16 +0530

    adding openerp properties and gitignore file

--- src/main/java/org/bahmni/feed/openerp/AtomFeedProperties.java
+++ src/main/java/org/bahmni/feed/openerp/AtomFeedProperties.java
@@ -1,15 +1,45 @@
 package org.bahmni.feed.openerp;
 
+import org.bahmni.openerp.web.OpenERPProperties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AtomFeedProperties {
+public class AtomFeedProperties implements OpenERPProperties {
 
     private @Value("${feed.generator.uri}") String feedUri;
+    private @Value("${openerp.host}") String host;
+    private @Value("${openerp.port}") int port;
+    private @Value("${openerp.database}") String database;
+    private @Value("${openerp.user}") String user;
+    private @Value("${openerp.password}") String password;
 
     public String getFeedUri() {
         return feedUri;
     }
 
+    @Override
+    public String getHost() {
+        return host;
+    }
+
+    @Override
+    public int getPort() {
+        return port;
+    }
+
+    @Override
+    public String getDatabase() {
+        return database;
+    }
+
+    @Override
+    public String getUser() {
+        return user;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
 }
