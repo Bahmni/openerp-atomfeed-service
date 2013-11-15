3656957ade1160fa4a19adb7dfea6bf4bc7a4f76
pchandra <pchandra@thoughtworks.com>
2013-11-15 10:01:04 +0530

    praveen|fixing test

--- openerp-atomfeed-service/src/test/java/org/bahmni/feed/openerp/testhelper/SampleEncounter.java
+++ openerp-atomfeed-service/src/test/java/org/bahmni/feed/openerp/testhelper/SampleEncounter.java
@@ -20,7 +20,7 @@ public class SampleEncounter {
     }
 
     public static String requestParams(){
-        return "{\"id\":\"f18b7270-0085-4196-a6e9-367ebcbfc890\",\"openERPOrders\":[{\"id\":\"26141644-468c-4e71-8d1c-8c17fd3b6df0\",\"productIds\":[\"4a3ad265-34f7-4fbb-9cd5-d52289afea7a\"],\"visitId\":\"294f02b4-78d4-4b3a-831b-40b667efe3b8\",\"type\":null,\"startDate\":null,\"description\":\"null 29/05/2013 05:59:23\"},{\"id\":\"3f9960ef-f141-4131-a4b5-23835d69671d\",\"productIds\":[\"2461e8c0-ef23-475f-934d-b8d8366d5e90\"],\"visitId\":\"294f02b4-78d4-4b3a-831b-40b667efe3b8\",\"type\":null,\"startDate\":null,\"description\":\"null 29/05/2013 05:59:23\"},{\"id\":\"f81749c4-4827-4374-be64-28e5f52513f5\",\"productIds\":[\"2461e8c0-ef23-475f-934d-b8d8366d5e90\"],\"visitId\":\"294f02b4-78d4-4b3a-831b-40b667efe3b8\",\"type\":null,\"startDate\":null,\"description\":\"null 29/05/2013 05:59:23\"}]}";
+        return "{\"id\":\"f18b7270-0085-4196-a6e9-367ebcbfc890D\",\"openERPOrders\":[{\"id\":\"26141644-468c-4e71-8d1c-8c17fd3b6df0A\",\"productIds\":[\"d5aa4078-ce50-46f5-a233-fd195ce28665\"],\"visitId\":\"294f02b4-78d4-4b3a-831b-40b667efe3b8A\",\"type\":\"REG\",\"startDate\":null,\"description\":\"REG 29/05/2013 05:59:23\"},{\"id\":\"3f9960ef-f141-4131-a4b5-23835d69671dA\",\"productIds\":[\"8fabd64a-ff9e-4fd6-8392-678a17779ffd\"],\"visitId\":\"294f02b4-78d4-4b3a-831b-40b667efe3b8A\",\"type\":\"REG\",\"startDate\":null,\"description\":\"REG 29/05/2013 05:59:23\"},{\"id\":\"f81749c4-4827-4374-be64-28e5f52513f5A\",\"productIds\":[\"2461e8c0-ef23-475f-934d-b8d8366d5e90\"],\"visitId\":\"294f02b4-78d4-4b3a-831b-40b667efe3b8A\",\"type\":\"REG\",\"startDate\":null,\"description\":\"REG 29/05/2013 05:59:23\"}]}" ;
     }
 
 }
