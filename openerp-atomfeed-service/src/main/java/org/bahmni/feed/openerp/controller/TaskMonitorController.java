7531d1a10a1f46b0be6942c056e113e8a62e75ae
Hemanth <hemanthfeb25@gmail.com>
2014-02-06 11:51:23 +0530

    Mujir, Hemanth | #1731 | added service endpoint to check openerp access to database

--- openerp-atomfeed-service/src/main/java/org/bahmni/feed/openerp/controller/TaskMonitorController.java
+++ openerp-atomfeed-service/src/main/java/org/bahmni/feed/openerp/controller/TaskMonitorController.java
@@ -2,6 +2,8 @@ package org.bahmni.feed.openerp.controller;
 
 import org.apache.log4j.Logger;
 import org.bahmni.feed.openerp.TasksMonitoringResponse;
+import org.bahmni.openerp.web.service.CustomerService;
+import org.bahmni.openerp.web.service.OpenERPService;
 import org.quartz.JobKey;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
@@ -12,26 +14,30 @@ import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("/tasks")
 // TODO : Mujir - this doesnt have any authentication right now. Change nagios when we add basic auth.
 public class TaskMonitorController {
+    private OpenERPService openERPService;
     private SchedulerFactoryBean schedulerFactoryBean;
 
     private static Logger logger = Logger.getLogger(TaskMonitorController.class);
 
     @Autowired
-    public TaskMonitorController(SchedulerFactoryBean schedulerFactoryBean) {
+    public TaskMonitorController(SchedulerFactoryBean schedulerFactoryBean, OpenERPService openERPService) {
         this.schedulerFactoryBean = schedulerFactoryBean;
+        this.openERPService = openERPService;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public @ResponseBody List<TasksMonitoringResponse> taskStatus() {
+    @RequestMapping(value = "/tasks", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    List<TasksMonitoringResponse> taskStatus() {
         List<TasksMonitoringResponse> monitoringResponses = new ArrayList<>();
         try {
             Scheduler scheduler = schedulerFactoryBean.getScheduler();
@@ -46,5 +52,11 @@ public class TaskMonitorController {
             logger.error(e);
         }
         return monitoringResponses;
+    }
+
+    @RequestMapping(value = "/customer", method = RequestMethod.GET, params = {"patientId"})
+    @ResponseBody
+    public Object[] search(@RequestParam String patientId) {
+        return openERPService.findCustomers(patientId);
     }
 }
