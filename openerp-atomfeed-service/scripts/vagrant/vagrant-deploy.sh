66910ac8e116a18bc1ca12fcb175d30fb5f9eb06
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-03-21 01:55:36 +0530

    Gurpreet| #1965| Refactored the Vagrant connection into vagrant_functions.sh

--- openerp-atomfeed-service/scripts/vagrant/vagrant-deploy.sh
+++ openerp-atomfeed-service/scripts/vagrant/vagrant-deploy.sh
@@ -1,15 +1,14 @@
 #!/bin/sh -x
 
+PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $PATH_OF_CURRENT_SCRIPT/vagrant_functions.sh
+
 #All config is here
 TEMP_ERP_FEED_SERVICE_WAR_FOLDER=/tmp/deploy_erp_feed_service
-MACHINE_IP=192.168.33.10
 SCRIPTS_DIR=scripts/vagrant
 SHELL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_BASE=$SHELL_DIR/../..
-KEY_FILE=~/.vagrant.d/insecure_private_key
 ERP_FEED_SERVICE_WAR=$PROJECT_BASE/target/openerp-atomfeed-service.war
-TIMEOUT="-o ConnectTimeout=5"
-
 
 if [[ ! -a $ERP_FEED_SERVICE_WAR ]]; then
     echo "----------------------------------------"
@@ -19,12 +18,12 @@ if [[ ! -a $ERP_FEED_SERVICE_WAR ]]; then
 fi
 
 # Setup environment
-ssh vagrant@$MACHINE_IP -i $KEY_FILE $TIMEOUT < $SCRIPTS_DIR/setup_environment.sh
+run_in_vagrant -f "$SCRIPTS_DIR/setup_environment.sh"
 
 # Copy ERP Atom Feed Service App War file to Vagrant tmp
-scp  -i $KEY_FILE $TIMEOUT $ERP_FEED_SERVICE_WAR vagrant@$MACHINE_IP:$TEMP_ERP_FEED_SERVICE_WAR_FOLDER
+scp_to_vagrant $ERP_FEED_SERVICE_WAR $TEMP_ERP_FEED_SERVICE_WAR_FOLDER
 
 
 #Deploy them from Vagrant /tmp to appropriate location
-ssh vagrant@$MACHINE_IP -i $KEY_FILE $TIMEOUT < $SCRIPTS_DIR/deploy_erp_feed_service_war.sh
+run_in_vagrant -f "$SCRIPTS_DIR/deploy_erp_feed_service_war.sh"
 
