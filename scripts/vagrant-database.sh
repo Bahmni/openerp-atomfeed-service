ae836d72dd894126843903bae80ad0bbbdcb4de1
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-03-24 11:14:07 +0530

    Gurpreet|#1965| Ensure that war file is copied to /packages/build dir before running liquibase migration in vagrant

--- scripts/vagrant-database.sh
+++ scripts/vagrant-database.sh
@@ -1,4 +1,9 @@
 #!/bin/sh -x
-MACHINE_IP=192.168.33.10
-KEY_FILE=~/.vagrant.d/insecure_private_key
-ssh vagrant@$MACHINE_IP -i $KEY_FILE "sudo su - jss -c 'cd /bahmni_temp/bahmni-openerp && ./run-liquibase.sh'"
+PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $PATH_OF_CURRENT_SCRIPT/../openerp-atomfeed-service/scripts/vagrant/vagrant_functions.sh
+
+run_in_vagrant -c "sudo rm -f /packages/build/openerp-atomfeed-service.war"
+run_in_vagrant -c "sudo chown jss:jss /packages/build"
+run_in_vagrant -c "sudo su - jss -c 'cp /Project/openerp-atomfeed-service/openerp-atomfeed-service/target/openerp-atomfeed-service.war /packages/build/'"
+run_in_vagrant -c "sudo su - jss -c 'cd /bahmni_temp/bahmni-openerp && ./run-liquibase.sh'"
+
