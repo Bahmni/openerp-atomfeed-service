effd39fb50e350f30b926599ea4de52ed0427224
Vinay Venu <vinayv@thoughtworks.com>
2014-11-03 18:55:20 +0530

    Vinay | #0 | Fix tomcat versions

--- openerp-atomfeed-service/scripts/deploy-war.sh
+++ openerp-atomfeed-service/scripts/deploy-war.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-scp openerp-atomfeed-service/target/openerp-atomfeed-service.war root@192.168.33.10:/home/jss/apache-tomcat-7.0.22/webapps/
+scp openerp-atomfeed-service/target/openerp-atomfeed-service.war root@192.168.33.10:/home/jss/apache-tomcat-8.0.12/webapps/
--- openerp-atomfeed-service/scripts/vagrant/deploy_erp_feed_service_war.sh
+++ openerp-atomfeed-service/scripts/vagrant/deploy_erp_feed_service_war.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -x
 
 TEMP_LOCATION=/tmp/deploy_erp_feed_service
-WAR_LOCATION=/home/jss/apache-tomcat-7.0.22/webapps
+WAR_LOCATION=/home/jss/apache-tomcat-8.0.12/webapps
 
 sudo rm -rf $WAR_LOCATION/openerp-atomfeed-service
 sudo su - jss -c "cp -f $TEMP_LOCATION/* $WAR_LOCATION"
