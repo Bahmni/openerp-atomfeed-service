3fb2d6c870910568f3beb2cc5ba7ef5cb2c38734
Vinay Venu <vinayv@thoughtworks.com>
2015-06-18 15:19:56 +0530

    Sudhakar, Vinay | Start using bahmni user, not jss

--- openerp-atomfeed-service/scripts/vagrant/deploy_erp_feed_service_war.sh
+++ openerp-atomfeed-service/scripts/vagrant/deploy_erp_feed_service_war.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -x
 
 TEMP_LOCATION=/tmp/deploy_erp_feed_service
-WAR_LOCATION=/home/jss/apache-tomcat-8.0.12/webapps
+WAR_LOCATION=/home/bahmni/apache-tomcat-8.0.12/webapps
 
 sudo rm -rf $WAR_LOCATION/openerp-atomfeed-service
-sudo su - jss -c "cp -f $TEMP_LOCATION/* $WAR_LOCATION"
+sudo su - bahmni -c "cp -f $TEMP_LOCATION/* $WAR_LOCATION"
