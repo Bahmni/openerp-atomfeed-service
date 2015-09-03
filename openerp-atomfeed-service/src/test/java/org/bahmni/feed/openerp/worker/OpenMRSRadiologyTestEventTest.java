cb8d3e2782c2fcaa750fd107f8b719a7ecc5090d
rnjn <runjan@gmail.com>
2015-09-03 16:58:44 +0530

    Added a lab order type feed listener - more changes

---
+++ openerp-atomfeed-service/src/test/java/org/bahmni/feed/openerp/worker/OpenMRSRadiologyTestEventTest.java
@@ -0,0 +1,12 @@
+package org.bahmni.feed.openerp.worker;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OpenMRSRadiologyTestEventTest {
+    @Test
+    public void shouldMapToOpenERPEvent() throws Exception {
+
+    }
+}
