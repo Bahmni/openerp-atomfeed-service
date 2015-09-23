6ca57aa8ee3232708db19b55cecdf978df9b4b25
hemanths <hemanths@INhemanths.local>
2015-09-23 12:59:34 +0530

    Hemanth | publishing event within transaction.

--- openerp-atomfeed-service/src/main/java/org/bahmni/feed/openerp/job/OpenERPEventsPublisherJob.java
+++ openerp-atomfeed-service/src/main/java/org/bahmni/feed/openerp/job/OpenERPEventsPublisherJob.java
@@ -1,11 +1,18 @@
 package org.bahmni.feed.openerp.job;
 
 import org.ict4h.atomfeed.server.repository.AllEventRecords;
+import org.ict4h.atomfeed.server.repository.AllEventRecordsOffsetMarkers;
 import org.ict4h.atomfeed.server.repository.AllEventRecordsQueue;
+import org.ict4h.atomfeed.server.repository.ChunkingEntries;
 import org.ict4h.atomfeed.server.repository.jdbc.AllEventRecordsJdbcImpl;
+import org.ict4h.atomfeed.server.repository.jdbc.AllEventRecordsOffsetMarkersJdbcImpl;
 import org.ict4h.atomfeed.server.repository.jdbc.AllEventRecordsQueueJdbcImpl;
+import org.ict4h.atomfeed.server.repository.jdbc.ChunkingEntriesJdbcImpl;
+import org.ict4h.atomfeed.server.service.NumberOffsetMarkerServiceImpl;
+import org.ict4h.atomfeed.server.service.OffsetMarkerService;
 import org.ict4h.atomfeed.server.service.publisher.EventRecordsPublishingService;
 import org.ict4h.atomfeed.server.transaction.AtomFeedSpringTransactionSupport;
+import org.ict4h.atomfeed.transaction.AFTransactionWorkWithoutResult;
 
 public class OpenERPEventsPublisherJob {
 
@@ -16,8 +23,18 @@ public class OpenERPEventsPublisherJob {
     }
 
     public void execute() {
-        AllEventRecords allEventRecords = new AllEventRecordsJdbcImpl(atomFeedSpringTransactionSupport);
-        AllEventRecordsQueue allEventRecordsQueue = new AllEventRecordsQueueJdbcImpl(atomFeedSpringTransactionSupport);
-        EventRecordsPublishingService.publish(allEventRecords, allEventRecordsQueue);
+        atomFeedSpringTransactionSupport.executeWithTransaction(new AFTransactionWorkWithoutResult() {
+            @Override
+            protected void doInTransaction() {
+                AllEventRecords allEventRecords = new AllEventRecordsJdbcImpl(atomFeedSpringTransactionSupport);
+                AllEventRecordsQueue allEventRecordsQueue = new AllEventRecordsQueueJdbcImpl(atomFeedSpringTransactionSupport);
+                EventRecordsPublishingService.publish(allEventRecords, allEventRecordsQueue);
+            }
+
+            @Override
+            public PropagationDefinition getTxPropagationDefinition() {
+                return PropagationDefinition.PROPAGATION_REQUIRED;
+            }
+        });
     }
 }
