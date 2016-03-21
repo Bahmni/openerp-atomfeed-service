1963078a0a40a021b959419c8c58b3231ba9132f
chethana <chethana@INchethana-2.local>
2016-03-21 12:28:49 +0530

    Chethan, Pankaj | #1045 | Refactored OpenMrsOrder.
    Moved OpenmrsConcept to seperate file so that it can be reused.

---
+++ openerp-atomfeed-service/src/main/java/org/bahmni/feed/openerp/domain/encounter/OpenMRSConcept.java
@@ -0,0 +1,42 @@
+package org.bahmni.feed.openerp.domain.encounter;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class OpenMRSConcept {
+    private String uuid;
+    private String name;
+    private String shortName;
+    private String units;
+    private String dataType;
+    private String conceptClass;
+    private boolean set;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public String getUnits() {
+        return units;
+    }
+
+    public String getDataType() {
+        return dataType;
+    }
+
+    public String getConceptClass() {
+        return conceptClass;
+    }
+
+    public boolean isSet() {
+        return set;
+    }
+}
--- openerp-atomfeed-service/src/main/java/org/bahmni/feed/openerp/domain/encounter/OpenMRSOrder.java
+++ openerp-atomfeed-service/src/main/java/org/bahmni/feed/openerp/domain/encounter/OpenMRSOrder.java
@@ -57,42 +57,4 @@ public class OpenMRSOrder {
         return getConcept() != null ? getConcept().getConceptClass() : null;
     }
 
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    public class OpenMRSConcept {
-        private String uuid;
-        private String name;
-        private String shortName;
-        private String units;
-        private String dataType;
-        private String conceptClass;
-        private boolean set;
-
-        public String getUuid() {
-            return uuid;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public String getShortName() {
-            return shortName;
-        }
-
-        public String getUnits() {
-            return units;
-        }
-
-        public String getDataType() {
-            return dataType;
-        }
-
-        public String getConceptClass() {
-            return conceptClass;
-        }
-
-        public boolean isSet() {
-            return set;
-        }
-    }
 }
