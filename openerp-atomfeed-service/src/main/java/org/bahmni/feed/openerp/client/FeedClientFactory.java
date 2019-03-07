1ce04d16c6b2216eadd3b243ca4f033d762f92ce
angshuman sarkar <angshuonline@gmail.com>
2019-03-07 15:35:53 +0530

    BAH-763 | fixed properties loading and externalization issues.  (#3)
    * BAH-763 | fixed properties loading and externalization issues. chaged wiring of spring contexts, creating specific context for atomfeed and jobs. The contexts loaded up from ContextLoaderListener were causing properties and beans to be loaded multiple times, also due to repeated component scans.
    
    * BAH-763 | moving Controller and Web end-pts to mvc config, loaded by dispatcher servlet. Also reverted back application listener, loading service context (previously was inline)

--- openerp-atomfeed-service/src/main/java/org/bahmni/feed/openerp/client/FeedClientFactory.java
+++ openerp-atomfeed-service/src/main/java/org/bahmni/feed/openerp/client/FeedClientFactory.java
@@ -28,10 +28,10 @@ public class FeedClientFactory {
 
     public AtomFeedClient getFeedClient(OpenERPAtomFeedProperties openERPAtomFeedProperties, AtomFeedSpringTransactionSupport transactionManager,
                                         String feedName, OpenERPClient openERPClient, AllFeeds allFeeds, AllMarkers allMarkers, AllFailedEvents allFailedEvents, Jobs jobName)  {
-        String feedUri = openERPAtomFeedProperties.getFeedUri(feedName);
+        String feedUri = openERPAtomFeedProperties.getFeedUriForJob(jobName);
         try {
             String urlPrefix = getURLPrefix(jobName,openERPAtomFeedProperties);
-            EventWorker eventWorker = workerFactory.getWorker(jobName, openERPAtomFeedProperties.getFeedUri(feedName), openERPClient,
+            EventWorker eventWorker = workerFactory.getWorker(jobName, feedUri, openERPClient,
                     urlPrefix);
             return new AtomFeedClient(allFeeds, allMarkers, allFailedEvents, atomFeedProperties(openERPAtomFeedProperties), transactionManager, new URI(feedUri), eventWorker) ;
         } catch (URISyntaxException e) {
