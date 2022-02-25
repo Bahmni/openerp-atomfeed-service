9c94fc9ac18a0272ce42020471af5eb5a932637e
Abinaya U <abinaya.u@thoughtworks.com>
2022-02-25 17:36:38 +0530

    BAH-1415 | refactored. shell scripts
    Co-authored-by: Arjun G <arjun.g@thoughtworks.com>
    Co-authored-by: Kavitha S <kavitha.s@thoughtworks.com>
    Co-authored-by: MOHANKUMAR T <mohan13081999@gmail.com>
    Co-authored-by: Umair Fayaz <omayrfayaz@gmail.com>

--- package/docker/scripts/docker_build.sh
+++ package/docker/scripts/docker_build.sh
@@ -4,6 +4,6 @@ set -xe
 cp core-1.0-SNAPSHOT.jar package/docker/bahmni-core.jar
 
 cd package
-#Building Docker images
+#Building Docker image
 ODOO_CONNECT_IMAGE_TAG=${BAHMNI_VERSION}-${GITHUB_RUN_NUMBER}
-docker build -t bahmni/odoo-connect:${ODOO_CONNECT_IMAGE_TAG} -f docker/Dockerfile  . --no-cache
+docker build -t bahmni/odoo-connect:${ODOO_CONNECT_IMAGE_TAG} -t bahmni/odoo-connect:latest -f docker/Dockerfile  . --no-cache
--- package/docker/scripts/docker_publish.sh
+++ package/docker/scripts/docker_publish.sh
@@ -2,14 +2,10 @@
 set -e
 ODOO_CONNECT_IMAGE_TAG=${BAHMNI_VERSION}-${GITHUB_RUN_NUMBER}
 echo ${DOCKER_HUB_TOKEN} | docker login -u ${DOCKER_HUB_USERNAME} --password-stdin
-echo "[INFO] Pushing build images"
+echo "[INFO] Pushing build image"
 docker push bahmni/odoo-connect:${ODOO_CONNECT_IMAGE_TAG}
 
-echo "[INFO] Tagging build images as SNAPSHOT Images"
-ODOO_CONNECT_SNAPSHOT_IMAGE_TAG=${BAHMNI_VERSION}-SNAPSHOT
-docker tag bahmni/odoo-connect:${ODOO_CONNECT_IMAGE_TAG} bahmni/odoo-connect:${ODOO_CONNECT_SNAPSHOT_IMAGE_TAG}
-
-echo "[INFO] Pushing SNAPSHOT images"
-docker push bahmni/odoo-connect:${ODOO_CONNECT_SNAPSHOT_IMAGE_TAG}
+echo "[INFO] Pushing latest image"
+docker push bahmni/odoo-connect:latest
 
 docker logout
