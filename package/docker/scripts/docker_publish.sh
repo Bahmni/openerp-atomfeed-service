37ff2b8b7a7a85c2ca28fb3d54be914e4665671c
MOHANKUMAR T <mohan13081999@gmail.com>
2022-02-28 11:14:37 +0530

    BAH-1415 | refactor. docker publish script to be generic
    Co-authored-by: Kavitha S <kavitha.s@thoughtworks.com>

--- package/docker/scripts/docker_publish.sh
+++ package/docker/scripts/docker_publish.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -e
-ODOO_CONNECT_IMAGE_TAG=${BAHMNI_VERSION}-${GITHUB_RUN_NUMBER}
-echo ${DOCKER_HUB_TOKEN} | docker login -u ${DOCKER_HUB_USERNAME} --password-stdin
-echo "[INFO] Pushing build image"
-docker push bahmni/odoo-connect:${ODOO_CONNECT_IMAGE_TAG}
 
-echo "[INFO] Pushing latest image"
-docker push bahmni/odoo-connect:latest
+echo ${DOCKER_HUB_TOKEN} | docker login -u ${DOCKER_HUB_USERNAME} --password-stdin
+docker push --all-tags $1
 
 docker logout
