0ffaae01870cb8ac621ea222a1e3e89bc439020f
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2023-11-01 13:53:50 +0530

    [Rahul] | BAH-3246 | Fix. Liquibase JAR Path And Package Updates

--- package/docker/scripts/run-liquibase.sh
+++ package/docker/scripts/run-liquibase.sh
@@ -2,10 +2,10 @@
 set -e -x
 
 CHANGE_LOG_TABLE="-Dliquibase.databaseChangeLogTableName=liquibasechangelog -Dliquibase.databaseChangeLogLockTableName=liquibasechangeloglock"
-LIQUIBASE_JAR="${WAR_DIRECTORY}/WEB-INF/lib/liquibase-core-2.0.3.jar"
+LIQUIBASE_JAR="${WAR_DIRECTORY}/WEB-INF/lib/liquibase-core-4.24.0.jar"
 DRIVER="org.postgresql.Driver"
 CREDS="--url=jdbc:postgresql://$ODOO_DB_SERVER:5432/odoo --username=$ODOO_DB_USERNAME --password=$ODOO_DB_PASSWORD"
-CLASSPATH="${WAR_DIRECTORY}/WEB-INF/lib/postgresql-42.6.0.jar"
+CLASSPATH="${WAR_DIRECTORY}/WEB-INF/lib/postgresql-42.2.23.jar"
 CHANGE_LOG_FILE="${WAR_DIRECTORY}/WEB-INF/classes/sql/db_migrations.xml"
 
 java $CHANGE_LOG_TABLE  -jar $LIQUIBASE_JAR --driver=$DRIVER --classpath=$CLASSPATH --changeLogFile=$CHANGE_LOG_FILE $CREDS update
