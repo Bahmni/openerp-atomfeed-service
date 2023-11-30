468829e55b127aa9bd924ca79dc460f0c4577591
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2023-11-30 09:48:17 +0530

    [Parvathy, Rahul] | BAH-3331 | Add. Un/Escape Character Parser
    Co-authored-by: Parvathy Babu <parvathy.babu@thoughtworks.com>

--- openerp-client/src/main/java/org/bahmni/openerp/web/request/builder/RequestBuilder.java
+++ openerp-client/src/main/java/org/bahmni/openerp/web/request/builder/RequestBuilder.java
@@ -5,6 +5,7 @@ import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+import org.apache.velocity.tools.generic.EscapeTool;
 import org.bahmni.openerp.web.OpenERPException;
 import org.bahmni.openerp.web.request.OpenERPRequest;
 import org.springframework.stereotype.Service;
@@ -22,6 +23,7 @@ public class RequestBuilder {
             velocityEngine.init();
             Template template = velocityEngine.getTemplate("request/template/xml_template.vm");
             VelocityContext context = new VelocityContext();
+            context.put("esc", new EscapeTool());
             context.put("parametersList", openERPRequest.getParameters());
             context.put("id", id);
             context.put("database", database);
