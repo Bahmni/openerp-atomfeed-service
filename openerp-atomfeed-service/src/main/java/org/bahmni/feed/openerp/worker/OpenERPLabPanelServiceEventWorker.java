693a82426c4fa6013d4e4dd4173d237b81d6b3b0
deeptirawat1510 <deepti.rawat@thoughtworks.com>
2023-12-12 12:33:05 +0530

    Revert "BAH-3375|Deepti,Parvathy|Add. LoggerFactory to lab test and lab panel event worker."
    This reverts commit 9ebfe34c9bc2e14904440e81be1e2986627c4ab0.

--- openerp-atomfeed-service/src/main/java/org/bahmni/feed/openerp/worker/OpenERPLabPanelServiceEventWorker.java
+++ openerp-atomfeed-service/src/main/java/org/bahmni/feed/openerp/worker/OpenERPLabPanelServiceEventWorker.java
@@ -1,7 +1,7 @@
 package org.bahmni.feed.openerp.worker;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.bahmni.feed.openerp.client.OpenMRSWebClient;
 import org.bahmni.feed.openerp.domain.labOrderType.OpenMRSLabOrderTypeEvent;
 import org.bahmni.feed.openerp.domain.labOrderType.OpenMRSLabPanelEvent;
@@ -17,7 +17,7 @@ import java.util.Map;
 
 public class OpenERPLabPanelServiceEventWorker implements EventWorker {
 
-    private static final Logger logger = LoggerFactory.getLogger(OpenERPLabPanelServiceEventWorker.class);
+    private static final Logger logger = LogManager.getLogger(OpenERPLabPanelServiceEventWorker.class);
     private final OpenERPContext openERPContext;
     private final String feedUrl;
     private final String odooURL;
