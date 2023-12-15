9731db2dc9cd06d6cd25cef43b6b4b00a76dd236
SanoferSameera <sanofersameera25@gmail.com>
2023-12-15 21:01:05 +0530

    BAH-3339 | Removed unused imports

--- openerp-client/src/main/java/org/bahmni/openerp/web/client/strategy/OpenERPContext.java
+++ openerp-client/src/main/java/org/bahmni/openerp/web/client/strategy/OpenERPContext.java
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
-import java.util.Vector;
-
 @Service
 @Lazy
 public class OpenERPContext {
