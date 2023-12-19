6d7bae6b5b67002155e1ca9a0748880a8592e00e
MOHANKUMAR T <31698165+mohan-13@users.noreply.github.com>
2023-12-19 17:47:13 +0530

    BAH-3380 | Add script to update openmrs and elis host and port for atomfeed sync (#95)

--- package/docker/scripts/start.sh
+++ package/docker/scripts/start.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -e
 
+echo "Waiting for ${ODOO_DB_SERVER}:5432 for 3600 seconds"
+sh wait-for.sh --timeout=3600 ${ODOO_DB_SERVER}:5432
 echo "[INFO] Substituting Environment Variables"
 envsubst < /opt/bahmni-erp-connect/etc/erp-atomfeed.properties.template > ${WAR_DIRECTORY}/WEB-INF/classes/erp-atomfeed.properties
+./update_openmrs_host_port.sh
 echo "[INFO] Running Liquibase migrations"
 sh /opt/bahmni-erp-connect/etc/run-liquibase.sh
 echo "[INFO] Starting Application"
