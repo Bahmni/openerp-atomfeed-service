39723092a4080a076d8168af17dbc57fc135d945
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2023-12-21 23:48:12 +0530

    [Rahul] | BAH-3374 | Add. Session Cookies To REST Post Calls

--- openerp-client/src/main/java/org/bahmni/openerp/web/client/strategy/implementation/OdooRESTClient.java
+++ openerp-client/src/main/java/org/bahmni/openerp/web/client/strategy/implementation/OdooRESTClient.java
@@ -4,14 +4,10 @@ import org.bahmni.openerp.web.OpenERPProperties;
 import org.bahmni.openerp.web.client.strategy.OpenERPClientStrategy;
 import org.bahmni.openerp.web.http.client.RestClient;
 import org.bahmni.openerp.web.request.OpenERPRequest;
-import org.bahmni.openerp.web.request.builder.Parameter;
 import org.bahmni.openerp.web.request.builder.RequestBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.UUID;
 
 @Service
 @Lazy
@@ -22,10 +18,11 @@ public class OdooRESTClient implements OpenERPClientStrategy {
     public OdooRESTClient(OpenERPProperties openERPProperties) {
         final String host = openERPProperties.getHost();
         final int port = openERPProperties.getPort();
+        final String database = openERPProperties.getDatabase();
         final String user = openERPProperties.getUser();
         final String password = openERPProperties.getPassword();
         final int connectionTimeoutInMilliseconds = openERPProperties.getConnectionTimeoutInMilliseconds();
-        restClient = new RestClient("http://" + host + ":" + port, user, password, connectionTimeoutInMilliseconds);
+        restClient = new RestClient("http://" + host + ":" + port, user, password, connectionTimeoutInMilliseconds, database);
     }
 
     @Override
