9477058468eb91519d8eb61acd1a955fbf91f8b2
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2024-06-24 13:05:35 +0530

    [Rahul] | BAH-3989 | Fix. Remove Script Based Liquibase Run

--- package/docker/scripts/start.sh
+++ package/docker/scripts/start.sh
@@ -6,7 +6,5 @@ sh wait-for.sh --timeout=3600 ${ODOO_DB_SERVER}:5432
 echo "[INFO] Substituting Environment Variables"
 envsubst < /opt/bahmni-erp-connect/etc/erp-atomfeed.properties.template > ${WAR_DIRECTORY}/WEB-INF/classes/erp-atomfeed.properties
 ./update_openmrs_host_port.sh
-echo "[INFO] Running Liquibase migrations"
-sh /opt/bahmni-erp-connect/etc/run-liquibase.sh
 echo "[INFO] Starting Application"
 java -jar $SERVER_OPTS $DEBUG_OPTS /opt/bahmni-erp-connect/lib/bahmni-erp-connect.jar
--- scripts/vagrant-database.sh
+++ scripts/vagrant-database.sh
@@ -6,4 +6,3 @@ export USER=bahmni
 run_in_vagrant -c "sudo rm -f /packages/build/openerp-atomfeed-service.war"
 run_in_vagrant -c "sudo chown ${USER}:${USER} /packages/build"
 run_in_vagrant -c "sudo su - ${USER} -c 'cp /bahmni/openerp-atomfeed-service/openerp-atomfeed-service/target/openerp-atomfeed-service.war /packages/build/'"
-run_in_vagrant -c "sudo su - ${USER} -c 'cd /bahmni_temp/bahmni-openerp && ./run-liquibase.sh'"
