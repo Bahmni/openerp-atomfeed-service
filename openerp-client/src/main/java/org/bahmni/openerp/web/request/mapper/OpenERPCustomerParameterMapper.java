7760dc3736b821d9759fc0242d03677b906a233b
lmphatsi <la.mphatsi@gmail.com>
2025-12-08 17:38:37 +0000

    Add age and sex to parameter mapper

--- openerp-client/src/main/java/org/bahmni/openerp/web/request/mapper/OpenERPCustomerParameterMapper.java
+++ openerp-client/src/main/java/org/bahmni/openerp/web/request/mapper/OpenERPCustomerParameterMapper.java
@@ -14,13 +14,19 @@ public class OpenERPCustomerParameterMapper {
     public static final String OPENERP_CUSTOMER_REF = "ref";
     public static final String OPENERP_CUSTOMER_VILLAGE = "village";
     public static final String CUSTOMER_RESOURCE = "res.partner";
+    public static final String OPENERP_CUSTOMER_SEX = "sex";
+    public static final String OPENERP_CUSTOMER_AGE = "age";
 
     public OpenERPRequest mapCustomerParams(Customer customer, String operation) {
         Parameter name = new Parameter(OPENERP_CUSTOMER_NAME,customer.getName(),"string") ;
         Parameter ref = new Parameter(OPENERP_CUSTOMER_REF,customer.getRef(),"string") ;
         Parameter village = new Parameter(OPENERP_CUSTOMER_VILLAGE,customer.getVillage(),"string") ;
+        Parameter sex = new Parameter(OPENERP_CUSTOMER_SEX, customer.getSex(), "string") ;
+        Parameter age = new Parameter(OPENERP_CUSTOMER_AGE,
+                              customer.getAge() == null ? "" : customer.getAge().toString(),
+                              "integer") ;
 
-        List<Parameter> parameters = Arrays.asList(name, ref, village);
+        List<Parameter> parameters = Arrays.asList(name, ref, village, sex, age);
         String resource = CUSTOMER_RESOURCE;
         OpenERPRequest request = new OpenERPRequest(resource, operation,parameters);
         return request;
